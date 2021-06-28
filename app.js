a5415ce1a984bcde0403d8c4e85e1fa869fc0daf
Naman <namana019@outlook.com>
2021-06-29 00:05:41 +0530

    Joingame endpoint fixed

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ app.use(cors());
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: '*',
+        origin: "*",
+       
+        // handlePreflightRequest: (req, res) => {
+        //     res.writeead(200, {
+        //         "Access-Control-Allow-Origin": "*",
+        //         "Access-Control-Allow-Methods": "GET,POST",
+        //         "Access-Control-Allow-Headers": "my-custom-header",
+        //         "Access-Control-Allow-Credentials": true
+        //     });
+        //     res.end();
+        // }
     }
 });
 
@@ -29,14 +39,16 @@ io.on('connection', (socket) => {
   
     // Connect the 2nd Player to the requested room. Show error if the room is full.
     socket.on('joingame', (data) => {
-      const room = io.nsps['/'].adapter.rooms[data.room];
-      if (room && room.length === 1) {
-        socket.join(data.room);
-        socket.broadcast.to(data.room).emit('creator', { name: data.name, icon: data.icon });
-        socket.emit('opponent', {});
-      } else {
-        socket.emit('error', { message: 'Room_Full' });
-      }
+    //   const room = io.nsps['/'].adapter.rooms[data.room];
+        const room = io.sockets.adapter.rooms.get(data.room);
+        if (room && room.size === 1) {
+            console.log(data)
+            socket.join(data.room);
+            socket.broadcast.to(data.room).emit('creator', { name: data.name, icon: data.icon });
+            socket.emit('opponent', {});
+        } else {
+            socket.emit('error', { message: 'Room_Full' });
+        }
     });
   
     // Handle the turn played by either player and notify the other.
